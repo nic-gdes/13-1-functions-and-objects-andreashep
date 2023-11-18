fe769a3144602c630990a235b66d15645347bc1b
Andrea Shepherd <118775368+andreashep@users.noreply.github.com>
2023-11-18 09:11:44 -0800

    html and js, assignment started and finished.

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-// Nothing here yet...
-// Make sure to link your JS in index.html!
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    describe() {
+            console.log("This " + this.year + " " + this.model + " is a " + this.make + ".")
+        }
+       
+}
+    
+const allCars = [];
+const car1 = new Car("Honda", "Passport", 1999);
+const car2 = new Car("GMC", "Sierra", 1995);
+const car3 = new Car("Mercury", "Cougar", 1968);
+const car4 = new Car("Ford", "F - 250", 1971);
+allCars.push(car1, car2, car3, car4);
+
+for (let i = 0; i < allCars.length; i++) {
+    allCars[i].describe();
+}
+
